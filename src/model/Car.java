0fad72cae83efbf763abd3ae0459a19b5e47c7c3
queer-lodgings-717 <grey7bin@gmail.com>
2025-11-25 12:13:11 -0800

    complete implementation with constructors

--- src/model/Car.java
+++ src/model/Car.java
@@ -2,12 +2,27 @@ package model;
 
 import java.time.Duration;
 
+/**
+ * Car vehicle type
+ * Inherits calculateFee logic from Vehicle parent class
+ */
 public class Car extends Vehicle {
+	
+	public Car() {
+		super();
+		setType(VehicleType.CAR);
+	}
+	
+	public Car(String plateNumber, String brand, String model, Color color) {
+		super(plateNumber, brand, model, color, VehicleType.CAR);
+	}
 
 	@Override
 	public double calculateFee(Duration duration) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
+	// Inherits both calculateFee(int) and calculateFee(Duration) from Vehicle
+	// No override needed - Vehicle.calculateFee handles CAR type via switch statement
 }
