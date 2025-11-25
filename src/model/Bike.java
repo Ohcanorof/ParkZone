e0c3b6b0ea8f27f0cad228c9a822757459bd25d9
queer-lodgings-717 <grey7bin@gmail.com>
2025-11-25 12:17:18 -0800

    complete implementation with constructors

--- src/model/Bike.java
+++ src/model/Bike.java
@@ -2,12 +2,27 @@ package model;
 
 import java.time.Duration;
 
+/**
+ * Bike (Motorcycle) vehicle type
+ * Inherits calculateFee logic from Vehicle parent class
+ */
 public class Bike extends Vehicle {
+	
+	public Bike() {
+		super();
+		setType(VehicleType.MOTORCYCLE);
+	}
+	
+	public Bike(String plateNumber, String brand, String model, Color color) {
+		super(plateNumber, brand, model, color, VehicleType.MOTORCYCLE);
+	}
 
 	@Override
 	public double calculateFee(Duration duration) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
+	// Inherits both calculateFee(int) and calculateFee(Duration) from Vehicle
+	// No override needed - Vehicle.calculateFee handles MOTORCYCLE type via switch statement
 }
